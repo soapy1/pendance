d79bc7d76021a9aa265973d4f5befb58dbc10dfc
sophia <scastellarin95@gmail.com>
2016-06-05 13:09:28 -0400

    Test for push buttons

--- pendance/pendance.ino
+++ pendance/pendance.ino
@@ -1,15 +1,33 @@
 int slide_g = 9;
 int slide_y = 8;
+int reset = 7;
+int panic = 6;
 
 void setup() {                
   pinMode(slide_g, INPUT);
   pinMode(slide_y, INPUT);  
+  pinMode(reset, INPUT);
+  pinMode(panic, INPUT);
   pinMode(13, OUTPUT);
 }
 
 void loop() {
 
     
+}
+
+void test_push_buttons() {
+  if (digitalRead(reset) == LOW) {
+    digitalWrite(13, HIGH);
+  }else{
+    digitalWrite(13, LOW);
+  }
+  
+  if (digitalRead(panic) == LOW) {
+    digitalWrite(13, HIGH);
+  }else{
+    digitalWrite(13, LOW);
+  } 
 }
 
 void test_slide() {
