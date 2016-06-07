9b5733c744277a29ff7c1e782201dc2c0c713bfb
sophia <scastellarin95@gmail.com>
2016-06-07 11:35:59 -0400

    Use only one led (opposed to tri colour led)

--- pendance/pendance.ino
+++ pendance/pendance.ino
@@ -5,8 +5,6 @@
  * - automatic activation of red mode activated after 15 s (from yellow mode)
  *     - click reset button to reset timer
  *     - if timer runs out activate red mode
- 
- * - use one led
  * - flash led when time is running out (blink at 5 s)
  */
 
@@ -14,9 +12,7 @@ int slide_g = 8;
 int slide_y = 7;
 int reset = 6;
 int panic_pin = 0;
-int red_led = 11;
-int blue_led = 10;
-int green_led = 9;
+int led = 9;
 
 int green_mode = false;
 int yellow_mode = false;
@@ -31,10 +27,8 @@ void setup() {
   pinMode(reset, INPUT);
   pinMode(panic_pin, INPUT_PULLUP);
   attachInterrupt(panic_pin, panic, FALLING);
-  pinMode(red_led, OUTPUT);
-  pinMode(blue_led, OUTPUT);
-  pinMode(green_led, OUTPUT);
-  
+  pinMode(led, OUTPUT);
+
   Serial.begin(9600);
 }
 
@@ -45,11 +39,7 @@ void loop() {
     enable_yellow_mode();
   }
  
-    if (digitalRead(slide_g) == LOW){
-    digitalWrite(13, HIGH);
-  }else if (digitalRead(slide_y) == LOW){
-    digitalWrite(13, LOW);
-  } 
+
 }
 
 void panic() {
@@ -59,29 +49,7 @@ void panic() {
   }
 }
 
-void setColor(int red, int green, int blue)
-{
-  red = 255 - red;
-  green = 255 - green;
-  blue = 255 - blue;
-  analogWrite(red_led, red);
-  analogWrite(green_led, green);
-  analogWrite(blue_led, blue);  
-}
-
-void blink_color(int red, int green, int blue, int times) {
-  for (int i=0; i<times; i++){ 
-    setColor(red, green, blue);
-    delay(500);
-    digitalWrite(red_led, LOW);
-    digitalWrite(blue_led, LOW);
-    digitalWrite(green_led, LOW);
-    delay(500);
-  }
-}
-
 void enable_panic_mode() {
-  blink_color(255, 0, 0, 4);
   num_panic = 0;
   red_mode = true;
   green_mode = false;
@@ -91,7 +59,6 @@ void enable_panic_mode() {
 }
 
 void enable_yellow_mode() {
-  blink_color(255, 255, 0, 4);
   PANIC_TRIGGER = 1; 
   yellow_mode = true;
   green_mode = false;
@@ -100,7 +67,6 @@ void enable_yellow_mode() {
 }
 
 void enable_green_mode() {
-  blink_color(0, 255, 0, 4);
   PANIC_TRIGGER = 3;
   green_mode = true;
   yellow_mode = false;
@@ -109,18 +75,10 @@ void enable_green_mode() {
 }
 
 void test_led() {
-  setColor(255, 0, 0);  // red
+  digitalWrite(led, HIGH);
   delay(1000);
-  setColor(0, 255, 0);  // green
+  digitalWrite(led, LOW);
   delay(1000);
-  setColor(0, 0, 255);  // blue
-  delay(1000);
-  setColor(255, 255, 0);  // yellow
-  delay(1000);  
-  setColor(80, 0, 80);  // purple
-  delay(1000);
-  setColor(0, 255, 255);  // aqua
-  delay(1000); 
 }
 
 void test_push_buttons() {
